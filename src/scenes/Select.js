62b2c3504e568c1c5aaa47f696937d2c5e95e785
Hao-Tien Cheng <hcheng40@ucsc.com>
2025-03-05 16:34:22 -0800

    update select.js

--- src/scenes/Select.js
+++ src/scenes/Select.js
@@ -5,19 +5,20 @@ class Select extends Phaser.Scene {
 
     create() {
         // text at the top
-        let selectConfig = {
+        this.selectConfig = {
             fontFamily: 'Comic Sans MS',
             fontSize: '60px',
             fontStyle: 'bold',
             color: '#FFFFFF',
             align: 'center'
         }
-        this.add.text(game.config.width / 2, 150, 'Select Your Character', selectConfig).setOrigin(0.5)
+        this.add.text(game.config.width / 2, 150, 'Select Your Character', this.selectConfig).setOrigin(0.5)
 
         // character list
         this.characters = [
             { key: 'apple', name: 'Apple' },    // index 0
             { key: 'peach', name: 'Peach' },    // index 1
+            // add new characters here
         ]
         this.Index = 0
         this.firstSelect = false
@@ -30,20 +31,20 @@ class Select extends Phaser.Scene {
         this.characterTexts = []
         let startX = 850
         let startY = 400
-        this.characters.forEach((chr, index) => {
-            let text = this.add.text(startX, startY + index * 100, chr.name, {
+        this.characters.forEach((chr, i) => {
+            let charTextConfig = this.add.text(startX, startY + i * 100, chr.name, {
                 fontFamily: 'Comic Sans MS',
                 fontSize: '45px',
                 color: '#FFFFFF',
                 align: 'center',
                 fixedWidth: 250
             })
-            this.characterTexts.push(text)
+            this.characterTexts.push(charTextConfig)
         })
 
         // text at the bottom
-        selectConfig.fontSize = '40px'
-        this.add.text(game.config.width / 2, 850, 'Press Z to Start', selectConfig).setOrigin(0.5)
+        this.selectConfig.fontSize = '40px'
+        this.add.text(game.config.width / 2, 850, 'Press Z to Start', this.selectConfig).setOrigin(0.5)
 
         // select
         this.input.keyboard.on('keydown-UP', () => { this.changeSelection(-1) })
@@ -62,18 +63,18 @@ class Select extends Phaser.Scene {
         if (!this.firstSelect) {
             this.Index = 0
             this.firstSelect = true
-            this.chrImage = this.add.sprite(450, 500, this.characters[this.Index].key).setScale(0.5)
+            this.chrImage = this.add.sprite(450, 500, this.characters[this.Index].key).setScale(0.75)
         } else {
             this.Index = Phaser.Math.Wrap(this.Index + keyPressed, 0, this.characters.length)
             this.chrImage.setTexture(this.characters[this.Index].key)
         }
 
         // update text style
-        this.characterTexts.forEach((text, index) => {
-            if (index == this.Index) {
-                text.setText(`< ${this.characters[index].name} >`).setStyle({ fontStyle: 'bold' })
+        this.characterTexts.forEach((text, i) => {
+            if (i == this.Index) {
+                text.setText(`< ${this.characters[i].name} >`).setStyle({ fontStyle: 'bold' })
             } else {
-                text.setText(this.characters[index].name).setStyle({ fontStyle: 'normal' })
+                text.setText(this.characters[i].name).setStyle({ fontStyle: 'normal' })
             }
         })
     }
